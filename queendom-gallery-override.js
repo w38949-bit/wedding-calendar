4714cf415cf48269f3bb72637fb55d70a10a50ea
w38949-bit <w38949@gmail.com>
2026-08-19 11:35:53 +0900

    Load final uploaded-HTML photo runtime

--- queendom-gallery-override.js
+++ queendom-gallery-override.js
@@ -1,7 +1,7 @@
 (()=>{
   if(document.querySelector('script[data-html-photo-loader="1"]'))return;
   const s=document.createElement('script');
-  s.src='html-sprite-photos.js?v=20260819-1114';
+  s.src='html-sprite-photos.js?v=20260819-1120';
   s.dataset.htmlPhotoLoader='1';
   s.defer=true;
   document.body.appendChild(s);
